765e47ce83047c2e1ba51917568ac15dccd65857
Rxx7 <allankhoshnaw7077@gmail.com>
2024-03-25 20:56:37 +0100

    Better comments

--- Assets/Player/Scripts/PlayerManager.cs
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -2,52 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Singleton class to manage player stats globally
+// Singleton class to manage player stats globally across the game
+// Ensures there is only one instance of PlayerManager throughout game lifecyle
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance { get; private set; }
 
-    [SerializeField] private float stamina = 100.0f;
-    [SerializeField] private float health = 100.0f;
-    [SerializeField] private float damage = 10.0f;
+    [SerializeField] private float stamina = 100.0f; // Players stamina
+    [SerializeField] private float health = 100.0f; // Players health
+    [SerializeField] private float damage = 10.0f; // Players attack damage
 
     // Called when instance is loaded and ensures there is only one instance of PlayerManager
     private void Awake()
     {
         if (Instance != null && Instance != this)
         {
-            // Gets rid of duplicates
-            Destroy(this);
+            Destroy(gameObject); // Destorys GameObject script is attatched to if there is a duplicate
         }
         else
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject); // Prevents PlayerManager from being destroyed when changing scenes
         }
     }
 
-    // Get stamina
+    // get and set player stamina
+    // Keeps stamina between 0 and 100
     public float Stamina
     {
         get { return stamina; }
         set
         {
-            // Keeps stamina between 0 and 100
             stamina = Mathf.Clamp(value, 0, 100);
         }
     }
 
-    // Get health
+    // get and set player health
+    // Keeps health between 0 and 100
     public float Health
     {
         get { return health; }
         set
         {
-            // Keeps health between 0 and 100
             health = Mathf.Clamp(value, 0, 100);
         }
     }
 
-    // Get damage
+    // get and set player damage
     public float Damage
     {
         get { return damage; }
