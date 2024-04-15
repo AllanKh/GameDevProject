dd660571458a4df99efb5619f4029034f4c8e774
Lioxsaf <Lizandro.cruz01@gmail.com>
2024-04-15 13:57:48 +0200

    Collision Logic
    Added a new sprite for Keys and added collision logic to both the potion and key prefab.

---
+++ Assets/Pickups/Scripts/PickupCollider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCollider : MonoBehaviour
+{
+    void Awake()
+    {
+        // Make Collider2D as a trigger
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D c2d)
+    {
+        //Destroy Potion if picked up.
+        if (c2d.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
