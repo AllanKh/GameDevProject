af309f3d2975523e98d3ced17d006bbfed6af802
DennisLeoRosdahl <Leo.d.rose@hotmail.com>
2024-04-28 23:31:59 +0200

    OptionsMenu
    The options are now fully functional!

--- Assets/UI-Menu/Scripts/GamePauseUI.cs
+++ Assets/UI-Menu/Scripts/GamePauseUI.cs
@@ -9,6 +9,7 @@ public class GamePauseUI : MonoBehaviour
 
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
 
     private void Awake()
@@ -20,6 +21,10 @@ public class GamePauseUI : MonoBehaviour
         mainMenuButton.onClick.AddListener(() => //When "mainMenuButton" is pressed, do this!
         {
             Loader.Load(Loader.Scene.MainMenuScene); //Go back to the Main Menu Scene.
+        });
+        optionsButton.onClick.AddListener(() => 
+        {
+            OptionsUI.Instance.Show();
         });
     }
 
--- Assets/UI-Menu/Scripts/OptionsUI.cs
+++ Assets/UI-Menu/Scripts/OptionsUI.cs
@@ -7,14 +7,17 @@ using UnityEngine.UI;
 public class OptionsUI : MonoBehaviour
 {
 
+    public static OptionsUI Instance {  get; private set; }
 
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
 
     private void Awake()
     {
+        Instance = this;
         soundEffectsButton.onClick.AddListener(() => //When this button is pressed, do this!
         {
             SoundManager.Instance.ChangeVolume();
@@ -25,11 +28,22 @@ public class OptionsUI : MonoBehaviour
             MusicManager.Instance.ChangeVolume();
             UpdateVisual();
         });
+        closeButton.onClick.AddListener(() =>
+        {
+            Hide();
+        });
     }
 
     private void Start()
     {
+        GameInput.Instance.OnGameUnpaused += GameInput_OnGameUnpaused;
         UpdateVisual();
+        Hide();
+    }
+
+    private void GameInput_OnGameUnpaused(object sender, System.EventArgs e) //Listening to this event lets you exit the options via ESC
+    {
+        Hide();
     }
 
     private void UpdateVisual() //Make sure the right digit is show on the button
@@ -39,5 +53,15 @@ public class OptionsUI : MonoBehaviour
 
     }
 
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
 
 }
