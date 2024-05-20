7b72bbeb1422ef9b0840f13dd1a1c08f186f44e9
DennisLeoRosdahl <Leo.d.rose@hotmail.com>
2024-05-20 21:48:32 +0200

    Potions inventory/Images
    Added images for the HPPotion Inventory that sync with the amount of HP potions held by the player.

--- Assets/UI-Menu/Scripts/PlayerHealthUI.cs
+++ Assets/UI-Menu/Scripts/PlayerHealthUI.cs
@@ -9,14 +9,97 @@ public class PlayerHealthUI : MonoBehaviour
     [SerializeField] private Image healthBarImage;
     [SerializeField] private Image staminaBarImage;
     [SerializeField] private Image manaBarImage;
-    [SerializeField] private TextMeshProUGUI potionAmount;
+    //Potions;
+    [SerializeField] private Image HPPotionOne;
+    [SerializeField] private Image HPPotionTwo;
+    [SerializeField] private Image HPPotionThree;
+    [SerializeField] private Image HPPotionFour;
+    [SerializeField] private Image HPPotionFive;
+    [SerializeField] private TextMeshProUGUI potionPlus;
 
+
+
+    private void Awake()
+    {
+        HPPotionOne.gameObject.SetActive(false);
+        HPPotionTwo.gameObject.SetActive(false);
+        HPPotionThree.gameObject.SetActive(false);
+        HPPotionFour.gameObject.SetActive(false);
+        HPPotionFive.gameObject.SetActive(false);
+        potionPlus.gameObject.SetActive(false);
+    }
 
     private void Update()
     {
         healthBarImage.fillAmount = PlayerManager.Instance.Health / 100;
         staminaBarImage.fillAmount = PlayerManager.Instance.Stamina / 100;
-        potionAmount.text = $"{PlayerManager.Instance.HeldPotions}";
-        //then mana?
+        Debug.Log(PlayerManager.Instance.HeldPotions);
+        
+        if (PlayerManager.Instance.HeldPotions > 5)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(true);
+            HPPotionThree.gameObject.SetActive(true);
+            HPPotionFour.gameObject.SetActive(true);
+            HPPotionFive.gameObject.SetActive(true);
+            potionPlus.gameObject.SetActive(true);
+        }
+        else if (PlayerManager.Instance.HeldPotions == 5)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(true);
+            HPPotionThree.gameObject.SetActive(true);
+            HPPotionFour.gameObject.SetActive(true);
+            HPPotionFive.gameObject.SetActive(true);
+            potionPlus.gameObject.SetActive(false);
+        }
+        else if (PlayerManager.Instance.HeldPotions == 4)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(true);
+            HPPotionThree.gameObject.SetActive(true);
+            HPPotionFour.gameObject.SetActive(true);
+            HPPotionFive.gameObject.SetActive(false);
+            potionPlus.gameObject.SetActive(false);
+        }
+        else if (PlayerManager.Instance.HeldPotions == 3)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(true);
+            HPPotionThree.gameObject.SetActive(true);
+            HPPotionFour.gameObject.SetActive(false);
+            HPPotionFive.gameObject.SetActive(false);
+            potionPlus.gameObject.SetActive(false);
+        }
+        else if (PlayerManager.Instance.HeldPotions == 2)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(true);
+            HPPotionThree.gameObject.SetActive(false);
+            HPPotionFour.gameObject.SetActive(false);
+            HPPotionFive.gameObject.SetActive(false);
+            potionPlus.gameObject.SetActive(false);
+        }
+        else if (PlayerManager.Instance.HeldPotions == 1)
+        {
+            HPPotionOne.gameObject.SetActive(true);
+            HPPotionTwo.gameObject.SetActive(false);
+            HPPotionThree.gameObject.SetActive(false);
+            HPPotionFour.gameObject.SetActive(false);
+            HPPotionFive.gameObject.SetActive(false);
+            potionPlus.gameObject.SetActive(false);
+        }
+        else if (PlayerManager.Instance.HeldPotions <= 0)
+        {
+            HPPotionOne.gameObject.SetActive(false);
+            HPPotionTwo.gameObject.SetActive(false);
+            HPPotionThree.gameObject.SetActive(false);
+            HPPotionFour.gameObject.SetActive(false);
+            HPPotionFive.gameObject.SetActive(false);
+            potionPlus.gameObject.SetActive(false);
+        }
+
+        //then mana? PlayerManager.Instance.HeldPotions
+
     }
 }
