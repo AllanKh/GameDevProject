8fab0ec174dbc5a3c400d0a2fb0ae99ef47b8477
DennisLeoRosdahl <Leo.d.rose@hotmail.com>
2024-05-20 23:20:44 +0200

    Boss Music
    Second Phase boss music added.

--- Assets/SFX - Music/Scripts/MusicManager.cs
+++ Assets/SFX - Music/Scripts/MusicManager.cs
@@ -6,7 +6,10 @@ using UnityEngine.Rendering;
 public class MusicManager : MonoBehaviour
 {
 
+    [SerializeField] private AudioClip musicClip;
+
     public static MusicManager Instance {  get; private set; }
+
 
     private AudioSource audioSource;
     private float volume = 0.3f;
@@ -16,6 +19,16 @@ public class MusicManager : MonoBehaviour
         Instance = this;
         audioSource = GetComponent<AudioSource>();
     }
+    private void Start()
+    {
+        BossAttack.secondPhase += BossAttack_secondPhase;
+    }
+
+    private void BossAttack_secondPhase(object sender, System.EventArgs e)
+    {
+        ChangeMusic(musicClip);
+    }
+
     public void ChangeVolume() //Change the game effects volume
     {
         volume += 0.1f;
@@ -30,4 +43,16 @@ public class MusicManager : MonoBehaviour
     {
         return volume;
     }
+
+
+    public void ChangeMusic(AudioClip music)  //This changes the music to whatever you want it to be, kinda.
+    {
+        Debug.Log("Test me :)");
+        audioSource.Stop();
+        audioSource.clip = music;
+        audioSource.Play();
+    }
+
+
+
 }
