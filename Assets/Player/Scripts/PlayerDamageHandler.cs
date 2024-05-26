668c48624e1f136f2c8cbf296b25c40c8b23387f
DennisLeoRosdahl <Leo.d.rose@hotmail.com>
2024-05-26 19:24:29 +0200

    Debug removed

--- Assets/Player/Scripts/PlayerDamageHandler.cs
+++ Assets/Player/Scripts/PlayerDamageHandler.cs
@@ -19,7 +19,7 @@ public class PlayerDamageHandler : MonoBehaviour
     {
         if (PlayerManager.Instance.Health <= 0)
         {
-            UnityEngine.Debug.Log("I think you died!");
+            
             //UnityEditor.EditorApplication.isPlaying = false;
 
             Time.timeScale = 0f;
