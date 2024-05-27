b38efbb7d4ba6d1a02d61e257a4ead1c63364890
Lioxsaf <Lizandro.cruz01@gmail.com>
2024-05-27 21:33:43 +0200

    Fixed Key spawning when all types of enemies are killed

--- Assets/Pickups/Scripts/SpawnKey.cs
+++ Assets/Pickups/Scripts/SpawnKey.cs
@@ -6,6 +6,8 @@ public class SpawnKey : MonoBehaviour
 {
 
     GameObject go;
+    GameObject go2;
+    GameObject go3;
     bool keyHasSpawned;
 
     private void Awake()
@@ -16,7 +18,9 @@ public class SpawnKey : MonoBehaviour
     private void FixedUpdate()
     {
         go = GameObject.FindWithTag("Skeleton");
-        if (go == null && keyHasSpawned == false)
+        go2 = GameObject.FindWithTag("FlyingEye");
+        go3 = GameObject.FindWithTag("SkeletonSpawner");
+        if (go == null && keyHasSpawned == false && go2 == null && go3 == null)
         {
             GetComponent<SpawnPickups>().SpawnKeyEvent();
             keyHasSpawned = true;
